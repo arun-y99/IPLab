2e4351d56f5c562d03cb325afe0b1b2ecccec01b
Srinivasa Arun Yeragudipati <38272686+arun-y99@users.noreply.github.com>
2020-03-11 14:03:20 +0530

    Add files via upload

---
+++ MarklistApp/WEB-INF/classes/Data.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+public class Data extends HttpServlet{
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html><head>WELCOME</head><body>");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","ssn");
+			out.println("<p>Connected</p><br><br>");
+			
+			String rid = request.getParameter("rid");
+			//out.println(rid);
+
+			Statement stmt = con.createStatement();
+			String sql = "select * from student";
+			ResultSet rs = stmt.executeQuery(sql);
+			String id,marks;
+			String name;
+			out.println("<table border='1'>");
+			out.println("<tr><th>Rollno</th><th>Name</th><th>Marks</th></tr>");	
+			while(rs.next()) {
+				id = rs.getString("rollno");
+				//out.println(id);
+				name = rs.getString("name");
+				marks = rs.getString("marks");
+				if(Integer.parseInt(id) == Integer.parseInt(rid)) { 
+					out.println("<tr><td>"+id+"</td><td>"+ name+"</td><td>"+marks+"</td></tr>");
+				}
+			}
+			out.println("</table></body></html>");
+			con.close();
+		} catch (Exception e) {
+			out.println("<p>Not Connected</p><br><br>");
+			out.println("</body></html>");
+		}
+	}
+}
+
